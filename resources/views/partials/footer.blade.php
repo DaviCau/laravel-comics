e4d14ed76ad0da1aeabf660927f7fb48083d2b6c
DaviCau <davide.cau.rn@gmail.com>
2021-07-14 10:43:47 +0200

    aggiunti header e footer senza link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,54 @@
 <footer>
-    
+    <div class="footer-top" style="background-image: url('{{ asset("img/footer-bg.jpg") }}')">
+        <div class="container footer-container">
+            {{-- <div class="links">
+                <ul v-for="(section, index) in footerLinks" :key="index">
+                    <li class="list-title">{{section.sectionTitle}}</li>
+                    <li v-for="(link, indexLink) in footerLinks[index].sectionLinks" :key="indexLink">
+                        <a :href="link.url">{{link.text}}</a>
+                    </li>
+                </ul>
+            </div> --}}
+            <div class="logo-bg">
+                <img src="{{ asset("img/dc-logo-bg.png") }}" alt="logo dc">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container footer-container">
+            <div class="sign-up">
+                <button>Sign-up now&excl;</button>
+            </div>
+            <div class="socials">
+                <ul class="social-links">
+                    <li class="list-title">Follow Us</li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset("img/footer-facebook.png") }}" alt="facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset("img/footer-twitter.png") }}" alt="twitter">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset("img/footer-youtube.png") }}" alt="youtube">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset("img/footer-pinterest.png") }}" alt="pinterest">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset("img/footer-periscope.png") }}" alt="periscope">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    
+    <div class="container header">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="logo dc">
+  
+        <div class="nav">
+            <ul>
+                {{-- <li v-for="(link, index) in links" :key=index>
+                    <a :href="link.url" :class="link.current ? 'active' : ''">{{link.text}}</a>
+                </li> --}}
+            </ul>
+        </div>
+    </div>
+    <div class="jumbotron" style="background-image: url('{{ asset("img/jumbotron.jpg") }}')"></div>
 </header>
